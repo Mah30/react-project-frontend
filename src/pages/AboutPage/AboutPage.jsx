040999fdbc0582f93387ceb7416c69ab516e487e
Mariah <alinemariah@hotmail.com>
2024-12-31 13:12:06 +0100

    add and change images

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -1,11 +1,35 @@
 import React from 'react';
+import background4 from '../../assets/images/background4.jpg';
+import mulherprogramandoF from '../../assets/images/mulherprogramandoF.png';
+import SearchBar from '../SearchBar';
 
 const AboutPage = () => {
+
+    const containerStyle = {
+        backgroundImage: `url(${background4}`, 
+        backgroundSize: 'cover', 
+        backgroundPosition: 'center', 
+        backgroundRepeat: 'no-repeat', 
+        height: '100vh', 
+        width: '100vw', 
+        display: 'flex',
+        flexDirection: 'column', 
+      };
+
+
     return ( 
-        <div>
+
+      
+
+        <div style={containerStyle}>
+
+            <SearchBar/>
+
             <h2>About this Project</h2>
             <p>This is a Webpage Project created with React for learning practice </p>
             <p>Created from: Mariah</p>
+
+            <img src={mulherprogramandoF} alt="woman programming" />
 
             <a href="https://github.com/Mah30" target='_blank'>GitHub</a>
             <a href="https://www.linkedin.com/in/alinemariah-webdesigner/" target='_blank'>Linkedin</a>
--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import notFoundImage from '../assets/images/notFoundImage.jpg';
+import notFoundImage from '../assets/images/notFoundImage.png';
 import SearchBar from './SearchBar';
 
 
@@ -14,15 +14,18 @@ const NotFoundPage = () => {
             <SearchBar/>
         
             <div style={{ textAlign: "center", marginTop: "50px" }}>
-                <h2 style={{color: "#16425b"}}>404 Not Found</h2>
-                <p style={{color: "#16425b"}}>Sorry, the page you're looking for doesn't exist.</p>
+                <h1 style={{color: "#16425b"}}>404 Not Found</h1>
+                <h3 
+                style={{color: "#16425b"}}>Sorry, the page you're looking for doesn't exist.</h3>
+                
                 <img
                 src={notFoundImage}
                 alt='Image to not found page'
                 style = {{maxWidth: "100%", height: "auto", }}
-                
                 />
+
                 <Link to ="/" style = {{color:"blue", textDecoration: "underline"}}>Back to Home Page</Link>
+                
             </div>
         </>
      );
