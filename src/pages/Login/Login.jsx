abf51790882d3d351ca5b8cedbb4aa8059032f4f
Mariah <alinemariah@hotmail.com>
2025-01-07 23:45:08 +0100

    login and register started submitForm add For what? I don't know

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,28 +1,75 @@
 import React, { useState } from 'react';
 
 
+
+const useForm = (initialState) => {
+    const [formValues, setFormValues] = useState(initialState);
+  
+    const handleChange = (event) => {
+      const { name, value } = event.target;
+      setFormValues({ ...formValues, [name]: value });
+    };
+  
+    return [formValues, handleChange];
+  };
+
+  
+
+
 const Login  = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+
+    const [formValues, handleChange] = useForm({ email: "", password: "" });
+
     
 
-    const handleSubmit = (event) => {
+    const handleSubmit = async (event) => {
         event.preventDefault(); //impede o carregamento da pagina
-        console.log("Email:", email);
-        console.log("Password", password);
-
-        if (email === "teste@email.com" && password === "123456"){
-            alert("Your login has been successful")
-        } else {
-            alert ()
-        }
+        try {
+            const response = await fetch.post("", {
+              email,
+              password,
+            });
+            localStorage.setItem("token", response.data.token);
+      alert("Login bem-sucedido!");
+    } catch (err) {
+      setError("Login falhou. Verifique suas credenciais.");
     }
+
+    console.log("Email:", formValues.email);
+    console.log("Password", formValues.password);
+  };
+
+
+        
+   
+
+
 
     return ( 
         <div>
             
-            <form>
             <h1>Login</h1>
+            <form onSubmit={handleSubmit}>
+                <input
+                type="email"
+                name="email"
+                value={formValues.email}
+                onChange={(event) => setEmail(event.target.value)}
+                placeholder="Type your Email here"  />
+
+                <input
+                type="password"
+                name="password"
+                value={formValues.password}
+                onChange={(event) => setPassword(event.target.value)}
+                placeholder="Type your password here"
+                />
+
+                <button type='submit'>Sign in</button>
+                
             </form>
         </div>
      );
--- src/pages/Login/Register.jsx
+++ src/pages/Login/Register.jsx
@@ -1,26 +1,43 @@
 import React from 'react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 
 const Register = () => {
+    const [firstname, setFirstName] = useState('')
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [city, setCity] = useState('');
+    const [country, setCounstry] = useState('');
     const [isChecked, setIsChecked] = useState('false');
 
 
     const handleSubmit = (event) => {
         event.prevenDefault();
 
-        console.log({ email, password, isChecked});
+        console.log({ name, email, password, isChecked});
 
     }
 
     return ( 
         <div>
             <h1>Register</h1>
+
+            <form onSubmit={handleSubmit}>
+                <label>
+                    <input
+                    onChange={event => setFirstName(event.target.value)} />
+                </label>
+
+                <button type='submit'>Submit</button>
+            
+            </form>    
+            
           
         </div>
      );
 }
  
 export default Register;
+
+
